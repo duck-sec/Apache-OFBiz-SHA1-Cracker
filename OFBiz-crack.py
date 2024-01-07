da3a1c43c9194bf33ba920247ae165745f0cf700
Ducksec <duck@ducksec.io>
2024-01-07 12:02:04 +0000

    added README.me

---
+++ OFBiz-crack.py
@@ -0,0 +1,56 @@
+import argparse
+import hashlib
+import base64
+import os
+
+def cryptBytes(hash_type, salt, value):
+    if not hash_type:
+        hash_type = "SHA"
+    if not salt:
+        salt = base64.urlsafe_b64encode(os.urandom(16)).decode('utf-8')
+    hash_obj = hashlib.new(hash_type)
+    hash_obj.update(salt.encode('utf-8'))
+    hash_obj.update(value)
+    hashed_bytes = hash_obj.digest()
+    result = f"${hash_type}${salt}${base64.urlsafe_b64encode(hashed_bytes).decode('utf-8').replace('+', '.')}"
+    return result
+
+def getCryptedBytes(hash_type, salt, value):
+    try:
+        hash_obj = hashlib.new(hash_type)
+        hash_obj.update(salt.encode('utf-8'))
+        hash_obj.update(value)
+        hashed_bytes = hash_obj.digest()
+        return base64.urlsafe_b64encode(hashed_bytes).decode('utf-8').replace('+', '.')
+    except hashlib.NoSuchAlgorithmException as e:
+        raise Exception(f"Error while computing hash of type {hash_type}: {e}")
+
+def main():
+    parser = argparse.ArgumentParser(description="Brute force Apache OFBiz SHA1 hashes. Just something you might want to do....")
+    parser.add_argument("--hash-string", help="Hash string in the format '$TYPE$SALT$HASH'", required=True)
+    parser.add_argument("--wordlist", help="Path to the wordlist file", default="/usr/share/wordlists/rockyou.txt", required=False)
+
+    args = parser.parse_args()
+
+    hash_components = args.hash_string.split('$')
+    hash_type, salt, findhash = hash_components[1], hash_components[2], hash_components[3]
+
+    attempts = 0
+
+    with open(args.wordlist, 'r', encoding='latin-1') as password_list:
+        print ("[+] Attempting to crack....")
+        for password in password_list:
+            attempts += 1
+            value = password.strip()
+            hashed_password = cryptBytes(hash_type, salt, value.encode('utf-8'))
+            if hashed_password == args.hash_string:
+                print(f'Found Password: {value}')
+                print(f'hash: {hashed_password}')
+                print(f'(Attempts: {attempts})')
+                print("[!] Super, I bet you could log into something with that!")
+                break
+        else:
+            print(f"[!] Password not found in the wordlist. :( (Attempts: {attempts})")
+
+if __name__ == "__main__":
+    main()
--- README.md
+++ README.md
@@ -1,1 +1,67 @@
 # Apache-OFBiz-SHA1-Cracker
+
+This script uses python hashlib to brute force Apache OFBiz SHA1 hashes.
+
+
+## Description
+
+This is essentially a simple reverse engineer of the java used to generate the string in the first place:
+
+``` java
+ public static String cryptBytes(String hashType, String salt, byte[] bytes) {
+        if (hashType == null) {
+            hashType = "SHA";
+        }
+        if (salt == null) {
+            salt = RandomStringUtils.random(new SecureRandom().nextInt(15) + 1, CRYPT_CHAR_SET);
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append("$").append(hashType).append("$").append(salt).append("$");
+        sb.append(getCryptedBytes(hashType, salt, bytes));
+        return sb.toString();
+    }
+
+    private static String getCryptedBytes(String hashType, String salt, byte[] bytes) {
+        try {
+            MessageDigest messagedigest = MessageDigest.getInstance(hashType);
+            messagedigest.update(salt.getBytes(UtilIO.getUtf8()));
+            messagedigest.update(bytes);
+            return Base64.encodeBase64URLSafeString(messagedigest.digest()).replace('+', '.');
+        } catch (NoSuchAlgorithmException e) {
+            throw new GeneralRuntimeException("Error while comparing password", e);
+        }
+    }
+```
+
+## Prerequisites
+
+Before running the exploit script, ensure that you have:
+
+- Python 3.x installed on your system.
+- An Apache OFBiz hash in the format '$TYPE$SALT$HASH'
+
+
+
+## Usage
+
+```
+usage: OFBiz-crack.py [-h] --hash-string HASH_STRING [--wordlist WORDLIST]
+OFBiz-crack.py: error: the following arguments are required: --hash-string
+```
+
+## Example
+
+```
+
+$python3 OFBiz-crack.py --hash-string '$SHA1$d$F_kthjQD8fhzOOl9K9ueEBamX7g=' --wordlist /usr/share/wordlists/rockyou.txt
+[+] Attempting to crack....
+Found Password: tigger
+hash: $SHA1$d$F_kthjQD8fhzOOl9K9ueEBamX7g=
+(Attempts: 25)
+[!] Super, I bet you could log into something with that!
+
+```
+
+## Disclaimer
+
+This code is provided for educational purposes as well as for use in legitimate, AUTHORISED, security testing. Do not use this shell to attempt to access any system which you do not have explicit permission to test or practice on.
